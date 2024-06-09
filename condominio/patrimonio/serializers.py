dc577acb31a0a5aca75a79edb6ff88ff27726e14
ArthurRufi <temparthur87@gmail.com>
2024-06-09 16:23:10 -0300

    branch serializes created

---
+++ condominio/patrimonio/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from patrimonio.models import PatrimonioPrivadoUtensilio
+
+
+class SerializersPatrimonioPrivado(serializers.Serializer):
+    id = serializers.IntegerField(read_only = True)
