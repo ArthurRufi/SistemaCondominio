ec89dacfb5d23f782cf98ce420b83273d4bc1a9f
ArthurRufi <temparthur87@gmail.com>
2024-06-16 23:10:40 -0300

    iurls de moradores iniciadas

--- condominio/moradores/urls.py
+++ condominio/moradores/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ConsultarListaCompletaMoradores
+from .views import ConsultarListaCompletaMoradores, ConsultarVisitantes
 
 
 urlpatterns = [
-    path('', ConsultarListaCompletaMoradores.as_view(), name='asdka')
+    path('', ConsultarListaCompletaMoradores.as_view(), name='asdka'),
+    path('visitantes/', ConsultarVisitantes.as_view(), name='akkak')
 ]
