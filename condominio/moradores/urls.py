82c126f7646f72a8b36f4e8c9ad565a45cf1227f
ArthurRufi <temparthur87@gmail.com>
2024-08-12 12:03:40 -0300

    criação dos modulos para testes em credenciais

--- condominio/moradores/urls.py
+++ condominio/moradores/urls.py
@@ -3,6 +3,6 @@ from .views import ConsultarListaCompletaMorador, ConsultarVisitante
 
 
 urlpatterns = [
-    path('', ConsultarListaCompletaMorador.as_view(), name='asdka'),
-    path('visitantes/', ConsultarVisitante.as_view(), name='akkak'),
+    path('', ConsultarListaCompletaMorador.as_view(), name='moradores'),
+    path('visitantes/', ConsultarVisitante.as_view(), name='visitantes-consulta'),
 ]
