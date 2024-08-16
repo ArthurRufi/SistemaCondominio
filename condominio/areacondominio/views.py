ebd9341114571ac64a0ebd7d64e5968c59a97e11
ArthurRufi <temparthur87@gmail.com>
2024-08-16 15:51:28 -0300

    commit marco para inicio das alterações finais da 1.0 em areas condominios

--- condominio/areacondominio/views.py
+++ condominio/areacondominio/views.py
@@ -14,6 +14,7 @@ from datetime import date
 class APIArea(APIView):
     permission_classes = [AllowAny]
     def get(self, request):
+        
         area = modelsArea .objects.all()
         serializers = SerializersArea(area, many=True)
         return Response(serializers.data)
